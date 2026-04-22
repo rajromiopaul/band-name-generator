aa6774951ee1365575cf56c4f38aca0772ffabf4
Raj Romio Paul <rajromiopaul@gmail.com>
2026-04-22 16:56:00 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,6 @@
+print("Welcome to the band name generator.")
+
+city = input("which city did you grow up in?\n")
+pet = input("What is the name of your pet?\n")
+
+print(f"Your band name could be {city} {pet}.")
